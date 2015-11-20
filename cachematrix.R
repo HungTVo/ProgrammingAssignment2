a39b45b6b53dbbb17ab5ce24f9df312524acef0e
Hung Vo <thanhhungqb@gmail.com>
2015-11-20 11:51:14 +0700

    Assignment submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## created by: thanhhungqb@gmail.com
+##
 
-## Write a short comment describing this function
+## This function make a cached matrix
+## include set, get matrix and set, get for Inv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function (y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInv <- function(invi) inv <<- invi
+  getInv <- function() inv
+  list(set=set, get=get, setInv=setInv, getInv=getInv)
 }
 
 
-## Write a short comment describing this function
+## This function use data from x (make by above function)
+## if inv found then get it (cached)
+## if inv not calculated, then calculate and save to x by x$setInv
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInv()
+    if (!is.null(inv)) {
+      return (inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setInv(inv)
+    return (inv)
 }
